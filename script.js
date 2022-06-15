a5e55b76e672e103f0997ed8872960d2e8984759
Dallas Palumbo <palumbodallas@gmail.com>
2022-06-15 15:16:03 -0400

    adding carosel

---
+++ script.js
@@ -0,0 +1,19 @@
+let cars = ["assets/mclaren.png", "assets/golden.jpg", "assets/mazda.jpg"];
+const bubbleButtons = document.querySelectorAll('.bubble');
+
+bubbleButtons[index].textContent = '●';
+
+document.querySelector(".img")
+let index = 2
+carhead.src = cars[index]
+
+// for (let i = 0; i < bubbleButtons.length; i++) {
+//     bubbleButtons[i].addEventListener('click', function (e) {
+//         index = i;
+//         image.src = images[index]
+//         for(let i = 0; i < bubbleButtons.length; i++){
+//          bubbleButtons[i].textContent = '○'
+//         bubbleButtons[index].textContent = '●'
+//         }
+//     })
+// }
