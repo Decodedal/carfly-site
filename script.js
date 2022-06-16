018270000bbea343b791f4c9945b152689bc1a1f
Dallas Palumbo <palumbodallas@gmail.com>
2022-06-16 00:33:20 -0400

    timer?

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-let cars = ["assets/mclaren.png", "assets/golden.jpg", "assets/mazda.jpg"];
+let cars = ["assets/mclaren.png", "assets/golden.jpg", "assets/mazda.jpg","assets/nicepic7.jfif","assets/nicecar5.jpg","assets/nicecar6.jpg"];
 const bubbleButtons = document.querySelectorAll('.bubble');
 
-bubbleButtons[index].textContent = '●';
+document.addEventListener("DOMContentLoaded",(e)=>{
+    intervalID = setInterval(startCount,1000)
+})
+console.log(time)
 
 document.querySelector(".img")
-let index = 2
+let index = 0
 carhead.src = cars[index]
+bubbleButtons[index].textContent = '●'
 
-// for (let i = 0; i < bubbleButtons.length; i++) {
-//     bubbleButtons[i].addEventListener('click', function (e) {
-//         index = i;
-//         image.src = images[index]
-//         for(let i = 0; i < bubbleButtons.length; i++){
-//          bubbleButtons[i].textContent = '○'
-//         bubbleButtons[index].textContent = '●'
-//         }
-//     })
-// }
+for (let i = 0; i < bubbleButtons.length; i++) {
+    bubbleButtons[i].addEventListener('click', function (e) {
+        index = i;
+        carhead.src = cars[index]
+        for(let i = 0; i < bubbleButtons.length; i++){
+         bubbleButtons[i].textContent = '○'
+        bubbleButtons[index].textContent = '●'
+        }
+    })
+}
